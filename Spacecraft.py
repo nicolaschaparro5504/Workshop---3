07b8c92b7804a33adbf58cc59fd987a5e00507e9
Nicolas Chaparro <nicolas.chaparrob@udea.edu.co>
2025-06-14 17:00:54 -0500

    Main class parameters adjustments

--- Spacecraft.py
+++ Spacecraft.py
@@ -1,7 +1,7 @@
 import math as m
 
 class Spacecraft:
-    def __init__(self, norad_id, name, orbital_altitude, orbital_period, mass, mission, country, region):
+    def __init__(self, norad_id, name, orbital_altitude, orbital_period, mass, mission, country):
 
         """
         Definition of basic parameters of an spacecraft
@@ -10,12 +10,10 @@ class Spacecraft:
 
         name: name of the spacecraft
         norad_id: number of identification of the spacecraft
-        orbital_altitude(km): altitude of operation
+        orbital_altitude(km): altitude of operation meassured from the surface of Earth
         orbital_period(hours): time that takes to complete an orbit
         mass(kg): initial mass of the spacecraft with full fuel
         country: country that deployed the spacecraft
-        region: region in which the spacecraft works
-
         """
         self.norad_id = norad_id
         self.name = name
@@ -24,4 +22,3 @@ class Spacecraft:
         self.mass = mass
         self.mission = mission
         self.country = country
-        self.region = region
