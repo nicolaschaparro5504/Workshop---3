e1854f4cc03842f42e16adc1b5c0bd87c95a8051
Nicolas Chaparro <nicolas.chaparrob@udea.edu.co>
2025-06-19 11:26:32 -0500

    Attaching the altitude control subsystem to the spacecraft

--- Spacecraft.py
+++ Spacecraft.py
@@ -1,8 +1,9 @@
 import math as m
 import time
 from power_subsystem import Power_Subsystem
+from altitude_control_subsystem import Altitude_Control_Subsystem
 class Spacecraft:
-    def __init__(self, norad_id, name, orbital_altitude, orbital_period, mass, mission, country):
+    def __init__(self, norad_id, name, orbital_altitude, orbital_period, mass, country):
 
         """
         Definition of basic parameters of an spacecraft
@@ -21,12 +22,30 @@ class Spacecraft:
         self.orbital_altitude = orbital_altitude
         self.orbital_period = orbital_period
         self.mass = mass
-        self.mission = mission
         self.country = country
 
         #Attaching the power subsystem
         self.power_subsystem = Power_Subsystem()
-    
+
+        self.altitude_control = Altitude_Control_Subsystem(orbital_altitude)
+        self.altitude_control.attach_power_system(self.power_subsystem)
+
+
     def get_battery_status(self):
-        print(f"[Battery] Current Percentage: {self.power_system.get_battery_level():.2f}%")
+        print(f"[Battery] Current Percentage: {self.power_subsystem.get_battery_level():.2f}%")
+
+    def change_altitude(self, new_altitude):
+        if new_altitude > 50:
+            self.altitude_control.update_altitude(new_altitude)
+        else:
+            print("[Altitude] Desired altitude is not possible")
     
+    def change_orientation(self, x, y, z):
+        self.altitude_control.update_orientation([x,y,z])
+
+    def report_status(self):
+        self.altitude_control.report_ACS()
+        self.get_battery_status()
+
+
+sc = Spacecraft(1332, "LEO", 200, 2, 400, "USA")
